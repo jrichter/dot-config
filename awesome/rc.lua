a18c76dcd1838f6029527b2230ca3768eb8107e6
Justin Richter <jrichter@jetfive.com>
2011-08-11 21:44:23 -0500

    added vicious submodule for awesome

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -41,13 +41,26 @@ layouts =
 }
 -- }}}
 
+ -- {{{ Tags
+ -- Define a tag table which will hold all screen tags.
+ tags = {
+   names  = { "www", "emacs", 3, 4, 5, 6, 7, 8, 9 },
+   layout = { layouts[5], layouts[5], layouts[1], layouts[1], layouts[1],
+              layouts[1], layouts[1], layouts[1], layouts[1]
+ }}
+ for s = 1, screen.count() do
+     -- Each screen has its own tag table.
+     tags[s] = awful.tag(tags.names, s, tags.layout)
+ end
+ -- }}}
+
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
-tags = {}
-for s = 1, screen.count() do
-    -- Each screen has its own tag table.
-    tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
-end
+-- tags = {}
+-- for s = 1, screen.count() do
+--     -- Each screen has its own tag table.
+--     tags[s] = awful.tag({ 1, 2, 3, 4, 5, 6, 7, 8, 9 }, s, layouts[1])
+-- end
 -- }}}
 
 -- {{{ Menu
